f0645e9d135b80fed27dffce0ab8fac5d212fe00
Levi <>
2024-02-13 17:33:03 -0600

    updated entry.client.tsx and entry.server.tsx

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -3,16 +3,25 @@
  * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
  * For more information, see https://remix.run/file-conventions/entry.client
  */
+import { RemixBrowser } from '@remix-run/react'
+import { startTransition, StrictMode } from 'react'
+import { hydrateRoot } from 'react-dom/client'
 
-import { RemixBrowser } from "@remix-run/react";
-import { startTransition, StrictMode } from "react";
-import { hydrateRoot } from "react-dom/client";
+const hydrate = () => {
+	startTransition(() => {
+		hydrateRoot(
+			document,
+			<StrictMode>
+				<RemixBrowser />
+			</StrictMode>
+		)
+	})
+}
 
-startTransition(() => {
-  hydrateRoot(
-    document,
-    <StrictMode>
-      <RemixBrowser />
-    </StrictMode>
-  );
-});
+if (typeof requestIdleCallback === 'function') {
+	requestIdleCallback(hydrate)
+} else {
+	// Safari doesn't support requestIdleCallback
+	// https://caniuse.com/requestidlecallback
+	setTimeout(hydrate, 1)
+}
